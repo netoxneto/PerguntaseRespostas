b5ab1c56d90553f18212a9d9f1d508f6563781e7
netoxneto <netoxneto@hotmail.com>
2022-02-05 15:24:08 -0300

    att s

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const sequelize = require('sequelize');
 
-const connection = new sequelize('nodeperguntas', 'root', 'Netomi*',{
+const connection = new sequelize('nodeperguntas', 'root', '12345678',{
     host: 'localhost',
     dialect: 'mysql'
 });
